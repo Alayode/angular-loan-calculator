a42c78e8b750a9572d9f14f4a8b872dd522d4f9f
Chris Samuel <ksamuel.chris@gmail.com>
2019-02-06 08:13:08 -0500

    converted html template into a STAGE-TWO  template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-loan-calculator';
+      enterLoanData = 'Enter Loan Data:';
+      calculatorTitle = 'Loan Balance, Cumulative Equity, and Interest Payments';
+      loanAmount = 'Amount of the loan ($):';
+      annualInterest = 'Annual Interest (%):';
+      repaymentPeriodInYears = 'Repayment period (years):';
+      zipcode = ' Zipcode (to find lenders):';
+      monthlyPayment = 'Monthly payment:';
+      currencySymbol = '$';
+      approximatePayments = 'Approximate Payment';
+      totalPayment = 'Total Payment: ';
+      totalInterest = 'Total interest: ';
+      sponsors = 'Sponsors: ';
+      lendersText = 'Apply for your loan with one of these fine lenders: ';
+      calculate = 'Calculate';
 }
