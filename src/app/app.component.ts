6678cdfc5c7a349641c674f8ba3026b0a1708353
Chris Samuel <ksamuel.chris@gmail.com>
2019-02-14 17:30:40 -0500

    used type assertions for handling the DOM Elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-      enterLoanData = 'Enter Loan Data:';
-      calculatorTitle = 'Loan Balance, Cumulative Equity, and Interest Payments';
-      loanAmount = 'Amount of the loan ($):';
-      annualInterest = 'Annual Interest (%):';
-      repaymentPeriodInYears = 'Repayment period (years):';
-      zipcode = ' Zipcode (to find lenders):';
-      monthlyPayment = 'Monthly payment:';
-      currencySymbol = '$';
-      approximatePayments = 'Approximate Payment';
-      totalPayment = 'Total Payment: ';
-      totalinterest = 'Total interest: ';
-      sponsors = 'Sponsors: ';
-      lendersText = 'Apply for your loan with one of these fine lenders: ';
-      calculate = 'Calculate';
+  enterLoanData = 'Enter Loan Data:';
+  calculatorTitle = 'Loan Balance, Cumulative Equity, and Interest Payments';
+  loanAmount = 'Amount of the loan ($):';
+  annualInterest = 'Annual Interest (%):';
+  repaymentPeriodInYears = 'Repayment period (years):';
+  zipcode = ' Zipcode (to find lenders):';
+  monthlyPayment = 'Monthly payment:';
+  currencySymbol = '$';
+  approximatePayments = 'Approximate Payment';
+  totalPayment = 'Total Payment: ';
+  totalinterest = 'Total interest: ';
+  sponsors = 'Sponsors: ';
+  lendersText = 'Apply for your loan with one of these fine lenders: ';
+  calculatorButton = 'Calculate';
+
+
+  think() {
+
+    const amount = document.getElementById('amount') as HTMLInputElement;
+    const apr = document.getElementById('apr') as HTMLInputElement;
+    const years = document.getElementById('years') as HTMLInputElement;
+    const zipcode = document.getElementById('zipcode') as HTMLInputElement;
+    const payment = document.getElementById('payment')as HTMLInputElement;
+    const total = document.getElementById('total') as HTMLInputElement;
+    const totalInterest = document.getElementById('totalinterest')as HTMLInputElement;
+    // console.log(amount, apr, years, zipcode, payment, total, totalInterest);
+  }
 }
