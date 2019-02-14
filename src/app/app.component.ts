082b31087857a137caff6a2a082551779b764394
Chris Samuel <ksamuel.chris@gmail.com>
2019-02-14 17:34:16 -0500

    refactored vanilla js calculate function name and tested to make sure our angular click directive still works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+const PERCENT_VALUE = 100;
+const totalMonthsInYear = 12;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,16 +24,31 @@ export class AppComponent {
   lendersText = 'Apply for your loan with one of these fine lenders: ';
   calculatorButton = 'Calculate';
 
-
-  think() {
-
+  calculate() {
+    // 1. Look up the input and output elements in the document
     const amount = document.getElementById('amount') as HTMLInputElement;
-    const apr = document.getElementById('apr') as HTMLInputElement;
+    const annualPercentageRate = document.getElementById(
+      'apr'
+    ) as HTMLInputElement;
     const years = document.getElementById('years') as HTMLInputElement;
     const zipcode = document.getElementById('zipcode') as HTMLInputElement;
-    const payment = document.getElementById('payment')as HTMLInputElement;
+    const payment = document.getElementById('payment') as HTMLInputElement;
     const total = document.getElementById('total') as HTMLInputElement;
-    const totalInterest = document.getElementById('totalinterest')as HTMLInputElement;
+    const totalInterest = document.getElementById(
+      'totalinterest'
+    ) as HTMLInputElement;
     // console.log(amount, apr, years, zipcode, payment, total, totalInterest);
+
+    const principal = parseFloat(amount.value);
+    const monthlyInterestRate =
+      parseFloat(annualPercentageRate.value) /
+      PERCENT_VALUE /
+      totalMonthsInYear;
+    const payments = parseFloat(years.value) * totalMonthsInYear;
+    
+      console.log(principal, monthlyInterestRate, payments);
+  
+
+    
   }
 }
